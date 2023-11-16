85da0e24f76224154d938306ea146e51f3248f65
Willyan Marques <willyanmarquesmelo@gmail.com>
2023-11-16 12:39:13 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch prisma-patch-#01
    Your branch is up to date with 'origin/prisma-patch-#01'.
    
    Changes to be committed:
    	modified:   .next/build-manifest.json
    	modified:   .next/cache/webpack/client-development/0.pack.gz
    	modified:   .next/cache/webpack/client-development/index.pack.gz
    	modified:   .next/cache/webpack/client-development/index.pack.gz.old
    	modified:   .next/cache/webpack/server-development/0.pack.gz
    	modified:   .next/cache/webpack/server-development/index.pack.gz
    	modified:   .next/cache/webpack/server-development/index.pack.gz.old
    	modified:   .next/server/_error.js
    	modified:   .next/server/middleware-build-manifest.js
    	modified:   .next/server/pages-manifest.json
    	modified:   .next/server/pages/_app.js
    	modified:   .next/server/pages/_document.js
    	modified:   .next/server/pages/_error.js
    	modified:   .next/server/pages/index.js
    	deleted:    .next/server/pages/user.js
    	modified:   .next/server/vendor-chunks/@swc.js
    	modified:   .next/static/chunks/_error.js
    	modified:   .next/static/chunks/main.js
    	modified:   .next/static/chunks/pages/_app.js
    	modified:   .next/static/chunks/pages/index.js
    	deleted:    .next/static/chunks/pages/user.js
    	modified:   .next/static/chunks/webpack.js
    	modified:   .next/static/development/_buildManifest.js
    	renamed:    .next/static/webpack/c60caba8f60a6a7e.webpack.hot-update.json -> .next/static/webpack/449d2766db89ea4b.webpack.hot-update.json
    	deleted:    .next/static/webpack/e6b01c91d6be45c7.webpack.hot-update.json
    	renamed:    .next/static/webpack/webpack.e6b01c91d6be45c7.hot-update.js -> .next/static/webpack/webpack.449d2766db89ea4b.hot-update.js
    	deleted:    .next/static/webpack/webpack.c60caba8f60a6a7e.hot-update.js
    	modified:   .next/trace

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e011adaf31d63016"; }
+/******/ 		__webpack_require__.h = function() { return "e808b7b18551319e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.449d2766db89ea4b.hot-update.js
+++ .next/static/webpack/webpack.449d2766db89ea4b.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e6b01c91d6be45c7"; }
+/******/ 	__webpack_require__.h = function() { return "e808b7b18551319e"; }
 /******/ }();
 /******/ 
 /******/ }
